562521f2e7642e79441650bea5b285d41c0b5ea1
Jaume Jiménez <jaume.jimenezforteza@metro-markets.de>
2023-08-11 15:30:57 +0200

    Added basic webpage content

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-// Add any JavaScript functionality here if needed
+function updateCountdown(targetDate) {
+    const daysElement = document.getElementById('days');
+    const hoursElement = document.getElementById('hours');
+    const minutesElement = document.getElementById('minutes');
+    const secondsElement = document.getElementById('seconds');
+
+    setInterval(() => {
+        const currentDate = new Date();
+        const timeDifference = targetDate - currentDate;
+
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        daysElement.textContent = days;
+        hoursElement.textContent = hours;
+        minutesElement.textContent = minutes;
+        secondsElement.textContent = seconds;
+    }, 1000);
+}
+
+const targetDate = new Date('2023-08-26 12:00:00');
+updateCountdown(targetDate);
